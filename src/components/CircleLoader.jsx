ae08860159302965818a9fc7dccbe92eacd0788d
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-05-04 01:53:41 +0600

    feat: Added CircleLoader.jsx component to display a loading animation while fetching data from the API and the website is waiting for a response.

---
+++ src/components/CircleLoader.jsx
@@ -0,0 +1,7 @@
+export default function CircleLoader() {
+  return (
+    <div className="lg:h-[700px] h-screen flex justify-center items-center">
+      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
