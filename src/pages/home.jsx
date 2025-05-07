28cf7b88fc5879e9e3639cf627fe5e95c185a921
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-05-07 22:46:56 +0600

    feat: add wish button and its feature.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,13 +8,36 @@ import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import CircleLoader from "../components/CircleLoader";
 
-const PaginationBooks = () => {
+export default function PaginationBooks() {
   const [results, setResults] = useState([]);
   const [mostDownload, setMostDownload] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(null);
   const [loading, setLoading] = useState(false);
   const [apiError, setApiError] = useState(false);
+  const [wishlists, setWishlists] = useState([]);
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("wishlists")) || [];
+    setWishlists(stored);
+  }, []);
+
+  console.log(wishlists);
+
+  const toggleWishlist = (book) => {
+    const exists = wishlists.some((item) => item.id === book.id);
+    let updated;
+    if (exists) {
+      updated = wishlists.filter((item) => item.id !== book.id);
+    } else {
+      updated = [...wishlists, book];
+    }
+    setWishlists(updated);
+    localStorage.setItem("wishlists", JSON.stringify(updated));
+    window.dispatchEvent(new Event("wishlistUpdated"));
+  };
+
+  const isInWishlist = (bookId) => wishlists.some((b) => b.id === bookId);
 
   const fetchData = (page) => {
     setLoading(true);
@@ -212,8 +235,28 @@ const PaginationBooks = () => {
                     </button>
                   </Link>
 
-                  <button className="text-pink-500 hover:text-pink-600 text-base transition">
-                    ❤️
+                  <button onClick={() => toggleWishlist(book)}>
+                    {isInWishlist(book.id) ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 -960 960 960"
+                        width="24px"
+                        fill="#EA3323"
+                      >
+                        <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z" />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 -960 960 960"
+                        width="24px"
+                        fill="#EA3323"
+                      >
+                        <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Z" />
+                      </svg>
+                    )}
                   </button>
                 </div>
               </div>
@@ -244,6 +287,4 @@ const PaginationBooks = () => {
       </div>
     </>
   );
-};
-
-export default PaginationBooks;
+}
